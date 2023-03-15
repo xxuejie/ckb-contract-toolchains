60079a3317fb13f07c712e9aad0db2c5456d2138
Xuejie Xiao <xxuejie@gmail.com>
2023-03-15 09:26:49 +0000

    Enhancements

--- build_rust.sh
+++ build_rust.sh
@@ -6,37 +6,43 @@ VERSION=$2
 
 export PATH=${GNU_ROOT}/bin:$PATH
 
-rm -rf starter
-mkdir starter
-cp rustfiles starter/
+PACKAGE=rust_${VERSION}
+
+rm -rf ${PACKAGE}
+mkdir ${PACKAGE}
+
+RUST_VERSION=$(cat rust/src/version)
+sed "s/##VERSION##/${RUST_VERSION}/" rustfiles.template > ${PACKAGE}/rustfiles
+cp install_rust.sh ${PACKAGE}
+cp install_rust_component.sh ${PACKAGE}
 
 cd lib-dummy-atomics
 make clean
 make CC=riscv64-ckb-elf-gcc AR=riscv64-ckb-elf-ar
-cp libdummyatomics.a ../starter
+cp libdummyatomics.a ../${PACKAGE}
 # make clean
 cd ..
 
 cd ckb-c-stdlib
 make clean
 make libdummylibc.a CC=riscv64-ckb-elf-gcc AR=riscv64-ckb-elf-ar
-cp libdummylibc.a ../starter
+cp libdummylibc.a ../${PACKAGE}
 # make clean
 cd ..
 
-tar czf starter.tar.gz starter
-rm -rf starter
-
 cd rust
-sed "s/^#compression-formats.*/compression-formats = \[\"gz\"\]/ ; s/^#channel.*/channel = \"nightly\"/" config-ckb.toml.example > config.toml
+sed "s/^#compression-formats.*/compression-formats = \[\"gz\"\]/ ; s/^#ignore-git.*/ignore-git = false/" config-ckb.toml.example > config.toml
 ./x build --target riscv64imac_zba_zbb_zbc_zbs-unknown-ckb-elf,x86_64-unknown-linux-gnu
 ./x dist --target riscv64imac_zba_zbb_zbc_zbs-unknown-ckb-elf,x86_64-unknown-linux-gnu
 cd ..
 
 mkdir -p dist-${VERSION}
-for f in $(cat rustfiles); do
-  cp rust/build/dist/$f dist-${VERSION}
+for f in $(cat ${PACKAGE}/rustfiles); do
+  tar xzf rust/build/dist/${f}.tar.gz -C ${PACKAGE}
 done
-mv starter.tar.gz dist-${VERSION}
+
+tar czf ${PACKAGE}.tar.gz ${PACKAGE}
+rm -rf ${PACKAGE}
+mv ${PACKAGE}.tar.gz dist-${VERSION}
 
 echo "Build completed!"
--- install_rust_component.sh
+++ install_rust_component.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 set -ex
 
-DIST_FILE=$(realpath $1)
+COMPONENT_PATH=$(realpath $1)
 INSTALL_DIR=$(realpath $2)
 TEMP_DIR=$(realpath $3)
 
-UNCOMPRESSED_PATH=$(basename -- ${DIST_FILE} .tar.gz)
-RUN_TMP_PATH=${UNCOMPRESSED_PATH}_runtmp
+RUN_TMP_PATH=$TEMP_DIR/_runtmp
 
-echo $DIST_FILE
-echo $UNCOMPRESSED_PATH
+rm -rf $RUN_TMP_PATH
+mkdir $RUN_TMP_PATH
 
-rm -rf $TEMP_DIR/$UNCOMPRESSED_PATH
-tar xzf $DIST_FILE -C $TEMP_DIR
-
-rm -rf $TEMP_DIR/$RUN_TMP_PATH
-mkdir $TEMP_DIR/$RUN_TMP_PATH
-
-cd $TEMP_DIR/$RUN_TEMP_PATH
-$TEMP_DIR/$UNCOMPRESSED_PATH/install.sh \
+cd $RUN_TEMP_PATH
+$COMPONENT_PATH/install.sh \
   --prefix=${INSTALL_DIR} \
   --sysconfdir=${INSTALL_DIR}/etc \
   --datadir=${INSTALL_DIR}/share \
@@ -28,5 +21,4 @@ $TEMP_DIR/$UNCOMPRESSED_PATH/install.sh \
   --mandir=${INSTALL_DIR}/share/man \
   --disable-ldconfig
 
-rm -rf $TEMP_DIR/$UNCOMPRESSED_PATH
-rm -rf $TEMP_DIR/$RUN_TMP_PATH
+rm -rf $RUN_TMP_PATH
