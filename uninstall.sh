6ea2a92b8b42216a1e237da7a8cfb8809535511f
Xuejie Xiao <xxuejie@gmail.com>
2023-03-31 06:28:18 +0000

    Update uninstall script to try apt-get remove first

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-set -ex
 
 # Uninstalling all components of ckb-contract-toolchain
+
+if command -v apt-get &> /dev/null
+then
+  sudo apt-get remove -y ckb-riscv-toolchain
+fi
 
 sudo rm -rf /usr/lib/ckb-toolchain
 sudo rm -rf /usr/local/bin/riscv64-ckb-elf-*
